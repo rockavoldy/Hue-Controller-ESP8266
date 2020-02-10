e0744c9cad9ada72f2bda47d0ce15a5094675859
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-02-10 15:14:04 +0700

    add colorpicker

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container mx-auto flex justify-center h-screen">
+      <div class="self-center border w-6/12 text-center">
+        <ColorPicker v-bind="color" @input="onInput"></ColorPicker>
+        <pre>{{ color }}</pre>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data() {
+    return {
+      color: {
+        hue: 20,
+        saturation: 100,
+        luminosity: 50,
+        alpha: 1
+      }
+    };
+  },
+  methods: {
+    onInput: function(hue) {
+      this.color.hue = hue;
+    }
   }
-}
+};
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/tailwind.css'
+import Vue from "vue";
+import App from "./App.vue";
+import "./assets/tailwind.css";
+import "./assets/colorpicker.css";
+import ColorPicker from "@radial-color-picker/vue-color-picker";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(ColorPicker);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: h => h(App)
+}).$mount("#app");
