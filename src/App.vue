949030b3824d477032038b582c6511a95284a343
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-03-03 23:50:50 +0700

    working

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,112 @@
 <template>
   <div id="app">
-    <div class="container mx-auto flex justify-center h-screen">
-      <div class="self-center border w-6/12 text-center">
-        <ColorPicker v-bind="color" @input="onInput"></ColorPicker>
-        <pre>{{ color }}</pre>
+    <div class="container mx-auto flex justify-center flex-wrap">
+      <div class="w-full text-center mt-20">
+        <div v-if="connected">
+          <h1 class="text-lg">Connected</h1>
+          <div class="rounded bg-gray-400 px-1 py-1 inline-block">
+            <span>
+              <input
+                :disabled="connected"
+                class="rounded-l bg-gray-200 px-2 py-1 border border-gray-200"
+                v-model="url"
+                type="text"
+                placeholder="ws://192.168.100.15:8000"
+            /></span>
+            <button
+              class="px-2 py-1 rounded-r bg-red-700 border border-red-700 text-white"
+              @click="disconnectWS()"
+            >
+              Disconnect
+            </button>
+          </div>
+        </div>
+        <div v-else>
+          <h1 class="text-lg">Disconnected</h1>
+          <div class="rounded bg-gray-400 px-1 py-1 inline-block">
+            <span>
+              <input
+                class="rounded-l bg-gray-200 px-2 py-1 border border-gray-200 hover:bg-gray-100 hover:underline focus:bg-gray-100 focus:underline"
+                v-model="url"
+                type="text"
+                placeholder="ws://192.168.100.15:8000"
+            /></span>
+            <button
+              class="px-2 py-1 rounded-r bg-gray-700 border border-gray-700 text-white"
+              @click="connectWebSocket()"
+            >
+              Connect
+            </button>
+          </div>
+        </div>
+        <br />
+      </div>
+      <div class="w-full flex justify-center align-middle">
+        <ColorPicker :disabled="!connected" :change="updateColor" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ColorPicker from "./components/ColorPicker";
+// import { Chrome } from "vue-color";
 export default {
   name: "App",
+  components: {
+    // Chrome,
+    ColorPicker
+  },
   data() {
     return {
+      url: "ws://192.168.100.15:8000",
       color: {
-        hue: 20,
-        saturation: 100,
-        luminosity: 50,
-        alpha: 1
-      }
+        h: 0,
+        s: 0,
+        v: 0
+      },
+      socket: null,
+      connected: false,
+      connecting: null
     };
   },
+  created() {
+    // this.connectWebSocket();
+  },
   methods: {
-    onInput: function(hue) {
-      this.color.hue = hue;
+    updateColor: function(event) {
+      this.color = event.color;
+      if (this.socket) {
+        this.sendColor(event.color);
+      }
+    },
+    connectWebSocket() {
+      const socket = new WebSocket(this.url);
+      socket.onclose = () => {
+        if (socket.readyState === socket.CONNECTING) {
+          this.connecting = setTimeout(() => {
+            this.connectWebSocket();
+          }, 1000);
+        }
+      };
+
+      socket.onopen = () => {
+        clearTimeout(this.connecting);
+        this.socket = socket;
+        this.connected = true;
+      };
+    },
+    disconnectWS() {
+      this.socket.close();
+      this.connected = false;
+    },
+    sendColor(color) {
+      const data = {
+        h: color.h,
+        s: color.s,
+        v: color.v
+      };
+      this.socket.send(JSON.stringify(data));
     }
   }
 };
--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
-import "./assets/colorpicker.css";
-import ColorPicker from "@radial-color-picker/vue-color-picker";
 
 Vue.config.productionTip = false;
-
-Vue.use(ColorPicker);
 
 new Vue({
   render: h => h(App)
