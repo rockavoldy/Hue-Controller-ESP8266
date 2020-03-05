c20cb8a2cc5503ac4bdfe6e6edc865ff12d691ef
Akhmad Maulana Akbar <akhmad.98@icloud.com>
2020-03-06 02:08:14 +0700

    project using PlatformIO

--- src/HueController.cpp
+++ src/HueController.cpp
@@ -1,7 +1,10 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
 #include <WebSocketsServer.h>
 #include <ArduinoJson.h>
+#include <ArduinoOTA.h>
 
 #define FASTLED_ESP8266_RAW_PIN_ORDER
 #define FASTLED_ESP8266_NODEMCU_PIN_ORDER
@@ -13,9 +16,11 @@
 #define HOSTNAME "HueController"
 
 // User configurable
-#define PORT 8000              // WebSocketServer port, default 8000
-#define STASSID "std.64"       // Router SSID, if you want to connect to your existing network
-#define STAPSK "h3ll0w0r1d"    // Router Password, change this too
+#define PORT 8000 // WebSocketServer port, default 8000
+#ifndef STASSID
+#define STASSID "std.64"    // Router SSID, if you want to connect to your existing network
+#define STAPSK "h3ll0w0r1d" // Router Password, change this too
+#endif
 #define SKIPSTA false          // Change to true if you want to skip connecting to existing router
 #define APSSID "HueController" // SSID of Access Point created by ESP8266, if can't connect to Router SSID
 #define APPSK "helloworld"     // Password of Access Point created by ESP8266
@@ -117,12 +122,56 @@ void setup()
         USE_SERIAL.print("IP Address: ");
         USE_SERIAL.println(WiFi.softAPIP());
     }
+    ArduinoOTA.onStart([]() {
+        String type;
+        if (ArduinoOTA.getCommand() == U_FLASH)
+        {
+            type = "sketch";
+        }
+        else
+        { // U_FS
+            type = "filesystem";
+        }
 
+        // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+        Serial.println("Start updating " + type);
+    });
+    ArduinoOTA.onEnd([]() {
+        Serial.println("\nEnd");
+    });
+    ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+        Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+    });
+    ArduinoOTA.onError([](ota_error_t error) {
+        Serial.printf("Error[%u]: ", error);
+        if (error == OTA_AUTH_ERROR)
+        {
+            Serial.println("Auth Failed");
+        }
+        else if (error == OTA_BEGIN_ERROR)
+        {
+            Serial.println("Begin Failed");
+        }
+        else if (error == OTA_CONNECT_ERROR)
+        {
+            Serial.println("Connect Failed");
+        }
+        else if (error == OTA_RECEIVE_ERROR)
+        {
+            Serial.println("Receive Failed");
+        }
+        else if (error == OTA_END_ERROR)
+        {
+            Serial.println("End Failed");
+        }
+    });
+    ArduinoOTA.begin();
     webSocket.begin();
     webSocket.onEvent(webSocketEvent);
 }
 
 void loop()
 {
+    ArduinoOTA.handle();
     webSocket.loop();
 }
